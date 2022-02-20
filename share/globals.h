c9aff6c645317d5cb14df09e115dad571acbfd33
Filip <f.volkanoski@gmail.com>
2022-02-20 23:14:48 +0100

    Added message structure

--- share/globals.h
+++ share/globals.h
@@ -1,5 +1,19 @@
 #pragma once
 
+/*
+* -----------------------------------------------------------------------------
+* General message structure:
+* [IDCMDRRRRCC]
+* Where:
+* [ - Message start character
+* ID - Message ID
+* CMD - Command bits (Length depends on message id)
+* RRRR - 4 bits reserved for future use
+* CC - Checksum
+* ] - Message end character
+* -----------------------------------------------------------------------------
+*/
+
 // Serial
 #define BAUD_RATE  115200
 #define PORT_NAME "COM1"
