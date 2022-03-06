fb5c0855b6fe1d1f3cf14f5378d3adb5fd353512
Filip <f.volkanoski@gmail.com>
2022-03-06 14:17:08 +0100

    Added doxygen documentation to the dbus modules

--- share/dbus/dbushelper.h
+++ share/dbus/dbushelper.h
@@ -4,20 +4,45 @@
 #include <QtDBus/QDBusInterface>
 #include <QtDBus/QDBusMessage>
 
+/*! \brief DbusHelper class
+ *  Simplifies the dbusmodule by parsing the dbus messages that are received.
+*/
 class DbusHelper : public QObject
 {
     Q_OBJECT
 
 public:
+    /*! \brief Default constructor.
+       *  @param[in] *parent The parent QObject.
+       *  @return none
+      */
     explicit DbusHelper(QObject *parent = nullptr);
 
 signals:
+    /*! \brief This signal is emitted when a new message is received through dbus.
+     *  @param[in] deviceId The device id which sent the message.
+     *  @param[in] msg The message.
+     *  @return none
+    */
     void frameReceived(const QString &deviceId, const QString &frame);
 
 public slots:
+    /*! \brief This slot is triggered when the dbus module receives a new message.
+     *  @param[in] message The message that is received.
+     *  @return none
+    */
     Q_SCRIPTABLE void receiveDirectFrame(const QDBusMessage &message);
+
+    /*! \brief This slot is triggered when the dbus module receives a new broadcast message.
+     *  @param[in] msg The message that is received.
+     *  @return none
+    */
     void receiveBroadcast(const QDBusMessage &msg);
 
 private:
+    /*! \brief Parses the received dbus message.
+     *  @param[in] msg The message that is to be parsed.
+     *  @return bool Returns true if message is parsed successfully.
+    */
     bool parseDbusMsg(const QDBusMessage &msg);
 };
--- share/dbus/dbusmodule.h
+++ share/dbus/dbusmodule.h
@@ -6,24 +6,67 @@
 class DbusHelper;
 class QDBusInterface;
 
+/*! \brief DbusModule class
+ *  The dbus module class, initiates dbus connections, sends and receives messages
+*/
 class DbusModule : public QObject
 {
     Q_OBJECT
 
 public:
+    /*! \brief Default constructor.
+     *  @param[in] parent The parent QObject.
+     *  @param[in] deviceId The deviceId for the dbus connection.
+     *  @return none
+    */
     explicit DbusModule(const QString &deviceId, QObject *parent = nullptr);
+
+    /*! \brief Default destructor, disconnects the dbus connection and deletes pointers.
+     *  @return none
+    */
     ~DbusModule();
 
+    /*! \brief Getter for m_moduleName.
+     *  @return QString Returns m_moduleName.
+    */
     QString name() const { return m_moduleName; }
+
+    /*! \brief Getter for m_isConnected.
+     *  @return bool Returns m_isConnected.
+    */
     bool isConnected() const { return m_isConnected; }
+
+    /*! \brief Connects the dbus module.
+     *  @return bool Returns true if connection is successfull.
+    */
     bool connect();
+
+    /*! \brief Disconnects the dbus module.
+     *  @return bool Returns true if disconnection is successfull.
+    */
     bool disconnect();
+
+    /*! \brief Sends a dbus message.
+     *  @param[in] destAddress Where to send the dbus message to.
+     *  @param[in] frame The message that will be sent.
+     *  @return bool Returns true if message is sent successfully.
+    */
     bool sendFrame(const QString &destAddress, const QString &frame);
 
 signals:
+    /*! \brief This signal is emitted when a new message is received through dbus.
+     *  @param[in] deviceId The device id which sent the message.
+     *  @param[in] frame The message.
+     *  @return none
+    */
     void frameReceived(const QString &deviceId, const QString &frame);
 
 private slots:
+    /*! \brief This slot is triggered when a new frame is received.
+     *  @param[in] deviceId The device id which sent the message.
+     *  @param[in] frame The message.
+     *  @return none
+    */
     void frameReceivedSlot(const QString &deviceId, const QString &frame);
 
 private:
