d06b4f5fc850abbb8d9910e894df49afc8ab93de
Filip <f.volkanoski@gmail.com>
2022-03-06 14:25:05 +0100

    Added doxygen documentation to the serial port modules

--- serial/src/serialmanager.h
+++ serial/src/serialmanager.h
@@ -7,19 +7,37 @@
 #include "../share/dbus/dbusmodule.h"
 #include "../share/dbus/dbushelper.h"
 
+/*! \brief SerialManager class
+ *  Manages the serial module class.
+*/
 class SerialManager : public QObject
 {
     Q_OBJECT
 
 public:
+    /*! \brief Default constructor. Initiates the dbus and serial modules.
+     *  @param[in] *parent The parent QObject.
+     *  @return none
+    */
     explicit SerialManager(QObject *parent = nullptr);
 
+    /*! \brief Default destrucotr, deletes pointers.
+    */
     ~SerialManager();
 
 private slots:
+    /*! \brief This slot is triggered when there is new data from the serial port.
+     *  @param[in] msg The message that is received.
+     *  @return none
+    */
     void dataReceived(QString msg);
 
 private:
+    /*! \brief The serial module pointer.
+    */
     SerialModule *_serialModule;
+
+    /*! \brief The dbus module pointer.
+    */
     DbusModule *_dbusModule;
 };
--- serial/src/serialmodule.h
+++ serial/src/serialmodule.h
@@ -3,23 +3,52 @@
 #include <QObject>
 #include <QtSerialPort/QSerialPort>
 
+/*! \brief SerialModule class
+ *  Initiates and maintains the serial port connection.
+*/
 class SerialModule : public QObject
 {
     Q_OBJECT
 
 public:
+    /*! \brief Default constructor. Initiates the serial port module.
+     *  @param[in] *parent The parent QObject.
+     *  @return none
+    */
     explicit SerialModule(QObject *parent = nullptr);
+
+    /*! \brief Default destrucotr, deletes pointers.
+    */
     ~SerialModule();
 
+    /*! \brief Opens the serial port connection.
+     *  @param[in] none
+     *  @return bool Returns true if opening is successfull.
+    */
     bool openPort();
 
 signals:
+    /*! \brief This signal is emitted when there is new data from the serial port.
+     *  @param[in] msg The message that is received.
+     *  @return none
+    */
     void dataReceived(QString msg);
 
 private slots:
+    /*! \brief This slot is triggered when there is new data to receive from the serial port.
+     *  @param[in] none
+     *  @return none
+    */
     void receive();
+
+    /*! \brief This slot is triggered when there is an error on the serial port.
+     *  @param[in] err The serial port error.
+     *  @return none
+    */
     void serialError(QSerialPort::SerialPortError err);
 
 private:
+    /*! \brief The serial port pointer.
+    */
     QSerialPort *_serialPort;
 };
